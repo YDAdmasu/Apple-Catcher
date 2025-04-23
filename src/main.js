1e1dc2fcceff9485ef5da0ab245c4857896231ae
YDAdmasu <yididiyaadmasu@gmail.com>
2025-04-24 00:20:33 +0300

    particles

--- src/main.js
+++ src/main.js
@@ -17,6 +17,9 @@ class GameScene extends Phaser.Scene {
     this.textTime
     this.timedEvent
     this.remainingTime
+    this.coinMusic
+    this.bgMusic
+    this.emitter
   }
   preload() {
     this.load.image("bg", "/assets/bg.png")
@@ -56,6 +59,14 @@ class GameScene extends Phaser.Scene {
       fill: "#000000"
     })
     this.timedEvent = this.time.delayedCall(3000, this.gameOver,[],this)
+    this.emitter =this.add.particles(0,0,"money",{
+      speed: 100,
+      gravityY:speedDown - 200,
+      scale:0.04,
+      duration:100,
+      emitting:false
+    })
+    this.emitter.startFollow(this.player,this.player.width/2,this.player.height/2,true)
   }
   update() {
     this.remainingTime=this.timedEvent.getRemainingSeconds()
@@ -80,6 +91,7 @@ class GameScene extends Phaser.Scene {
   }
   targetHit(){
     this.coinMusic.play()
+    this.emitter.start()
     this.target.setY(0);
     this.target.setX(this.getRandomX());
     this.points ++;
