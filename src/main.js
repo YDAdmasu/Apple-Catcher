b950c5256788c836aef3739251af5307e3e9b451
YDAdmasu <yididiyaadmasu@gmail.com>
2025-04-24 00:08:41 +0300

    sfx

--- src/main.js
+++ src/main.js
@@ -17,21 +17,18 @@ class GameScene extends Phaser.Scene {
     this.textTime
     this.timedEvent
     this.remainingTime
-    this.coinMusic
-    this.bgMusic
   }
   preload() {
     this.load.image("bg", "/assets/bg.png")
     this.load.image("basket", "/assets/basket.png")
     this.load.image("apple", "/assets/apple.png")
-    this.load.audio("coin","/assets/coin.mp3")
-    this.load.audio("coin","/assets/bgMusic.mp3")
+    this.load.audio("coin" ," /assets/coin.mp3")
+    this.load.audio("bgMusic" , "/assets/bgMusic.mp3")
   }
   create() {
     this.coinMusic = this.sound.add("coin")
     this.bgMusic = this.sound.add("bgMusic")
     this.bgMusic.play()
-
     this.add.image(0, 0, "bg").setOrigin(0, 0)
     this.player = this.physics.add
       .image(0, sizes.height - 100, "basket")
@@ -58,7 +55,7 @@ class GameScene extends Phaser.Scene {
       font: "25px Arial",
       fill: "#000000"
     })
-    this.timedEvent = this.time.delayedCall(30000,this.gameOver,[],this)
+    this.timedEvent = this.time.delayedCall(3000, this.gameOver,[],this)
   }
   update() {
     this.remainingTime=this.timedEvent.getRemainingSeconds()
